157157a87c12a169215520a713ee4a3a0cd5b4a5
johannes <johannes.muljana@ait.nsw.edu.au>
2021-06-23 10:11:23 +1000

    Added app name in app.js

--- App.js
+++ App.js
@@ -3,19 +3,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    return ( 
+    <View> 
+      <Text>Money Tracker</Text>
     </View>
-  );
-}
+    )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    }
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+    });
